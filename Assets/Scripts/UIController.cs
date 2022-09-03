6cf91d191f1268a07fa2f5c41716a59c4101eedd
MuryanBKS <muryanbks+github@gmail.com>
2022-09-03 21:14:53 +0800

    Update UIController.cs

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,11 +11,6 @@ public class UIController : MonoBehaviour
     [SerializeField]
     private Text timeText;
 
-    private void Start()
-    {
-        SetTimeText(270);
-    }
-
     public void SetVehicleLife(float value)
     {
         vehicleLife.fillAmount = value;
